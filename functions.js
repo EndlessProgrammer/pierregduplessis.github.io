9c30b3fb1b3abd6883e16629dacbe4f7304206d4
Pierre Du Plessis <55995172+EndlessProgrammer@users.noreply.github.com>
2021-01-12 14:23:08 +0200

    Changes to website

--- functions.js
+++ functions.js
@@ -1,45 +1,11 @@
 //---------------------------------------Scrolling function---------------------------------------------
 var lastScrollTop;
-var scrollLock;
 var pageHeight;
-
-window.onscroll = function() {myFunction(null)};
-
-function myFunction(hash) {
-    // Using jQuery's animate() method to add smooth page scroll
-    var st = document.body.scrollTop || document.documentElement.scrollTop;
-    
-    if(hash != null && !scrollLock)
-    {
-      scrollLock = true;
-     $('html, body').scrollTop = $(hash).offset().top;
-     lastScrollTop = $(hash).offset().top;
-     setTimeout(() => { scrollLock = false;  }, 70);
-    }
-    else{
-    if (st > lastScrollTop && !scrollLock){
-      lastScrollTop = pageHeight + st;
-      scrollLock = true;
-      $('html, body').animate({
-        scrollTop: pageHeight + st
-      }, 800, function() {scrollLock = false;} );
-    }
-
-    if(st < lastScrollTop && !scrollLock){
-      lastScrollTop = st - pageHeight;
-      scrollLock = true;
-      $('html, body').animate({
-        scrollTop: st - pageHeight
-      }, 800, function() {scrollLock = false;} );
-    }
-  }
-}
 
 window.onresize = function() {pageHeight = window.innerHeight;}
 
     $(document).ready(function(){
       lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      scrollLock = false;
       
       pageHeight = window.innerHeight;
       // Add smooth scrolling to all links
@@ -53,10 +19,14 @@ window.onresize = function() {pageHeight = window.innerHeight;}
           // Store hash
           var hash = this.hash;
 
-          myFunction(hash);
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800);
           
           window.location.hash = hash;
        
         } // End if
       });
     });
+
+  
